fafa3dfd437d725cb5335314b199dccd1bdcc43d
Gobinda Das <gobinda168@gmail.com>
2020-01-24 19:44:39 +0530

    Updates

---
+++ precache-manifest.527567ef5c9d710e07d31dd6e15a4c26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc83bfbac94912ff367c91686a5dde0",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "d28144f14afc2604fea8",
+    "url": "/demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "72dad9b3f57b74d627bd",
+    "url": "/demo/static/js/2.f2e33c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/demo/static/js/2.f2e33c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d28144f14afc2604fea8",
+    "url": "/demo/static/js/main.ed775c0a.chunk.js"
+  },
+  {
+    "revision": "0f60a368fa5848e65e88",
+    "url": "/demo/static/js/runtime-main.d882a6f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
